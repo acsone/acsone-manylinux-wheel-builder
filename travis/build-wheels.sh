040279a20834993e9e0de04cba16fcacd86355e1
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-05-22 15:50:37 +0200

    build in a temp wheelhouse (audit wheel repair copies to final loc)

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -10,10 +10,10 @@ cat \
   | sort | uniq > /io/requirements.txt
 
 # Compile wheels
-PYBIN=/opt/python/$PY_VER/bin
-${PYBIN}/pip wheel -r /io/requirements.txt -w /io/wheelhouse/
+PYBIN=/opt/python/${PY_VER}/bin
+${PYBIN}/pip wheel -r /io/requirements.txt -w /tmp/wheelhouse/
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in /tmp/wheelhouse/*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
