3053eb1c3664f815c7be389f420cb3f292eb6284
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-05-22 16:00:36 +0200

    do not auditwheel repair pure wheels

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -14,6 +14,8 @@ PYBIN=/opt/python/${PY_VER}/bin
 ${PYBIN}/pip wheel -r /io/requirements.txt -w /tmp/wheelhouse/
 
 # Bundle external shared libraries into the wheels
-for whl in /tmp/wheelhouse/*.whl; do
+mkdir /io/wheelhouse
+cp -a /tmp/wheelhouse/*.whl /io/wheelhouse
+for whl in /tmp/wheelhouse/*-${PY_VER}-linux_$(uname -i).whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
