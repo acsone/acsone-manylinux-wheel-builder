216055c7c8b13bcf76077c5745d7ecb74dde2bf3
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-05-22 15:22:45 +0200

    select python version in the travis matrix

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,20 +4,14 @@ set -e -x
 # Install system packages required by our libraries
 # TODO
 
-# We build for these python versions
-PYABITAGS="cp27-cp27mu"
-
 # Enumerate all we need to build
-REQS=/io/requirements.txt
 cat \
   /io/requirements-oca.txt \
-  | sort | uniq > $REQS
+  | sort | uniq > /io/requirements.txt
 
 # Compile wheels
-for PYABITAG in $PYABITAGS; do
-    PYBIN=/opt/python/$PYABITAG/bin
-    ${PYBIN}/pip wheel -r $REQS -w /io/wheelhouse/
-done
+PYBIN=/opt/python/$PY_VER/bin
+${PYBIN}/pip wheel -r /io/requirements.txt -w /io/wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
