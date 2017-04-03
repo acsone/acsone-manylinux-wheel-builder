832b0052fe7079f2bbf19ed2bc846c68d19934ed
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-04-03 18:12:37 +0200

    openssl 1.1.0 won't build easily on centos 5
    so let's stick to 1.0.2k which is officially supported

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ wget ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -O cyrus-sasl.t
 wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.44.tgz -O openldap.tgz
 wget http://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
 wget http://web.mit.edu/kerberos/dist/krb5/1.14/krb5-1.14.3.tar.gz -O krb5.tgz
-wget https://www.openssl.org/source/openssl-1.1.0e.tar.gz -O openssl.tgz
+wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz -O openssl.tgz
 wget https://ftp.postgresql.org/pub/source/v9.6.2/postgresql-9.6.2.tar.gz -O postgresql.tgz
 wget ftp://xmlsoft.org/libxml2/LATEST_LIBXML2 -O libxml2.tgz
 wget ftp://xmlsoft.org/libxml2/LATEST_LIBXSLT -O libxslt.tgz
