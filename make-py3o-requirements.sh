496c7bc6950408192e270b99a68059633a4b8528
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-04-04 16:47:04 +0200

    update requirements*.txt

--- make-py3o-requirements.sh
+++
@@ -1,3 +0,0 @@
-wget -O /tmp/r1 https://bitbucket.org/xcgd/py3oserver_docker/raw/default/sources/pip_reqs.txt
-wget -O /tmp/r2 https://bitbucket.org/xcgd/py3o.fusion/raw/default/sources/pip_reqs.txt
-cat /tmp/r1 /tmp/r2 | sort | uniq > requirements-py3o.txt
---
+++ update-requirements.sh
@@ -0,0 +1,5 @@
+wget -O requirements-py3o-renderserver.txt https://bitbucket.org/xcgd/py3oserver_docker/raw/default/sources/pip_reqs.txt
+wget -O requirements-py3o-fusion.txt https://bitbucket.org/xcgd/py3o.fusion/raw/default/sources/pip_reqs.txt
+wget -O requirements-odoo8.txt https://raw.githubusercontent.com/odoo/odoo/8.0/requirements.txt
+wget -O requirements-odoo9.txt https://raw.githubusercontent.com/odoo/odoo/9.0/requirements.txt
+wget -O requirements-odoo10.txt https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
