8bfa81e5c0231ae3d7f96f12c0157f0d5f888f69
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-08-28 21:55:16 +0200

    use curl instead of wget for download
    Because wget seems to choke on travis with ftp downloads.

--- download.sh
+++ download.sh
@@ -3,13 +3,13 @@ set -e -x
 rm -fr downloads
 mkdir downloads
 pushd downloads
-wget -nv https://github.com/apple/cups/releases/download/v2.2.8/cups-2.2.8-source.tar.gz -O cups.tgz
-wget -nv ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -O cyrus-sasl.tgz
-wget -nv ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.46.tgz -O openldap.tgz
-wget -nv http://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
-wget -nv http://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -O krb5.tgz
-wget -nv https://www.openssl.org/source/openssl-1.0.2p.tar.gz -O openssl.tgz
-wget -nv https://ftp.postgresql.org/pub/source/v9.6.10/postgresql-9.6.10.tar.gz -O postgresql.tgz
-wget -nv ftp://xmlsoft.org/libxml2/LATEST_LIBXML2 -O libxml2.tgz
-wget -nv ftp://xmlsoft.org/libxml2/LATEST_LIBXSLT -O libxslt.tgz
+curl -sS https://github.com/apple/cups/releases/download/v2.2.8/cups-2.2.8-source.tar.gz -o cups.tgz
+curl -sS ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -o cyrus-sasl.tgz
+curl -sS ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.46.tgz -o openldap.tgz
+curl -sS http://zlib.net/zlib-1.2.11.tar.gz -o zlib.tgz
+curl -sS http://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -o krb5.tgz
+curl -sS https://www.openssl.org/source/openssl-1.0.2p.tar.gz -o openssl.tgz
+curl -sS https://ftp.postgresql.org/pub/source/v9.6.10/postgresql-9.6.10.tar.gz -o postgresql.tgz
+curl -sS ftp://xmlsoft.org/libxml2/LATEST_LIBXML2 -o libxml2.tgz
+curl -sS ftp://xmlsoft.org/libxml2/LATEST_LIBXSLT -o libxslt.tgz
 popd
