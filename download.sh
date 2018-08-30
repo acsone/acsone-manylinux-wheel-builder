ca46a9f9ec6a10ab1e8c3a0b0821c92874309fe4
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-08-30 21:47:53 +0200

    Latest cyrus-sasl does not build as is

--- download.sh
+++ download.sh
@@ -5,9 +5,7 @@ mkdir downloads
 pushd downloads
 # cups 2.2.8 does not build on this old images, do stick to 2.2.3
 wget -nv https://github.com/apple/cups/releases/download/v2.2.3/cups-2.2.3-source.tar.gz -O cups.tgz
-# ftp not working on travis?
-#wget -nv ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -O cyrus-sasl.tgz
-wget -nv https://github.com/cyrusimap/cyrus-sasl/archive/cyrus-sasl-2.1.27-rc7.tar.gz -O cyrus-sasl.tgz
+wget -nv ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -O cyrus-sasl.tgz
 wget -nv https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.4.46.tgz -O openldap.tgz
 wget -nv https://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
 wget -nv https://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -O krb5.tgz
