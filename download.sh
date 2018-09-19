e576d747d99d9522aedf72ad883a16d8a1d47117
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-09-19 22:55:01 +0200

    Fix libxml2/libxslt source location
    The git archives miss the configure script.

--- download.sh
+++ download.sh
@@ -12,6 +12,6 @@ wget -nv https://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
 wget -nv https://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -O krb5.tgz
 wget -nv https://www.openssl.org/source/openssl-1.0.2p.tar.gz -O openssl.tgz
 wget -nv https://ftp.postgresql.org/pub/source/v9.6.10/postgresql-9.6.10.tar.gz -O postgresql.tgz
-wget -nv https://github.com/GNOME/libxml2/archive/v2.9.8.tar.gz -O libxml2.tgz
-wget -nv https://github.com/GNOME/libxslt/archive/v1.1.32.tar.gz -O libxslt.tgz
+wget -nv http://xmlsoft.org/sources/libxml2-2.9.8.tar.gz -O libxml2.tgz
+wget -nv http://xmlsoft.org/sources/libxslt-1.1.32.tar.gz -O libxslt.tgz
 popd
