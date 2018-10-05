aeda3b45db8cf0e1b35d8d1b24f7e6c6a05a0b04
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-10-05 08:54:26 +0200

    Update requirements for Oodo 12

--- update-requirements.sh
+++ update-requirements.sh
@@ -7,5 +7,6 @@ wget -O requirements-odoo10.txt https://raw.githubusercontent.com/odoo/odoo/10.0
 cat requirements-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_-\.]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq > requirements-latest.txt
 
 wget -O requirements3-odoo11.txt https://raw.githubusercontent.com/odoo/odoo/11.0/requirements.txt
+wget -O requirements3-odoo12.txt https://raw.githubusercontent.com/odoo/odoo/12.0/requirements.txt
 wget -O requirements3-odoomaster.txt https://raw.githubusercontent.com/odoo/odoo/master/requirements.txt
 cat requirements3-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_-\.]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq > requirements3-latest.txt
