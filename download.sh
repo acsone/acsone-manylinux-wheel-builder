36195044e184e8296521aa8367d75b852a91feaa
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-12-08 13:16:27 +0100

    Update openssl and postgres

--- download.sh
+++ download.sh
@@ -10,8 +10,8 @@ cp ../vendor/cyrus-sasl-2.1.26.tar.gz cyrus-sasl.tgz
 wget -nv https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.4.46.tgz -O openldap.tgz
 wget -nv https://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
 wget -nv https://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -O krb5.tgz
-wget -nv https://www.openssl.org/source/openssl-1.0.2p.tar.gz -O openssl.tgz
-wget -nv https://ftp.postgresql.org/pub/source/v9.6.10/postgresql-9.6.10.tar.gz -O postgresql.tgz
+wget -nv https://www.openssl.org/source/openssl-1.0.2q.tar.gz -O openssl.tgz
+wget -nv https://ftp.postgresql.org/pub/source/v11.1/postgresql-11.1.tar.gz -O postgresql.tgz
 wget -nv http://xmlsoft.org/sources/libxml2-2.9.8.tar.gz -O libxml2.tgz
 wget -nv http://xmlsoft.org/sources/libxslt-1.1.32.tar.gz -O libxslt.tgz
 popd
