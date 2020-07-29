89abcd4718af20e74644c8d68b43b612e8392b78
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-07-29 18:33:38 +0200

    Limit max pycups version for python2

--- update-requirements.sh
+++ update-requirements.sh
@@ -3,6 +3,7 @@ wget -O requirements-odoo8.txt https://raw.githubusercontent.com/odoo/odoo/8.0/r
 wget -O requirements-odoo9.txt https://raw.githubusercontent.com/odoo/odoo/9.0/requirements.txt
 wget -O requirements-odoo10.txt https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
 cat requirements-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq > requirements-latest.txt
+sed -i "s/pycups/pycups<2/" requirements-latest.txt
 
 wget -O requirements3-odoo11.txt https://raw.githubusercontent.com/odoo/odoo/11.0/requirements.txt
 wget -O requirements3-odoo12.txt https://raw.githubusercontent.com/odoo/odoo/12.0/requirements.txt
