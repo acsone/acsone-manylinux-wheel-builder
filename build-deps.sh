e8f8eb12c8c0598652f75abdae4c2ab8c2e6479d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-07-20 13:31:44 +0200

    On manylinux 2014 we can use more deps from yum
    The base image is supported with security updates.

--- build-deps.sh
+++ build-deps.sh
@@ -15,13 +15,11 @@ cd build
 
 special_echo "some yum install..."
 
-# since CentOS 5 is obsolete, use the vault repo
-if [ "${MANYLINUX_VER}" == "manylinux1_x86_64" ] ; then
-    sed -i'' -e 's/^mirrorlist/#mirrorlist/' -e 's,^#baseurl=http://mirror.centos.org,baseurl=http://vault.centos.org,' /etc/yum.repos.d/CentOS-Base.repo
-fi
-
 # build tools
 yum install -y groff
+
+# ubiquitous zlib and openssl
+yum install -y zlib-devel openssl-devel
 
 # libjpeg and libtiff required for Pillow
 yum install -y libtiff-devel libjpeg-devel
@@ -30,81 +28,17 @@ yum install -y libtiff-devel libjpeg-devel
 yum install -y libffi-devel
 
 # recent cups
-special_echo "cups"
-rm -fr cups-*
-tar zxvf $DOWNLOADS/cups.tgz
-pushd cups-*
-./configure --prefix=/usr/local
-make
-make install
-popd
+yum install -y cups-devel
 
-# recent zlib
-special_echo "zlib"
-rm -fr zlib-*
-tar zxvf $DOWNLOADS/zlib.tgz
-pushd zlib-*
-./configure --prefix=/usr/local
-make
-make install
-popd
+# krb5 for postgres and openldap
+yum install -y krb5-devel
 
-# recent openssl
-special_echo "openssl"
-rm -fr openssl-*
-tar zxf $DOWNLOADS/openssl.tgz
-pushd openssl-*
-./config --prefix=/usr/local zlib shared
-make
-make install
-popd
+# openldap for python-openldap
+yum install -y openldap-devel
 
-# recent cyrus-sasl
-special_echo "cyrus-sasl"
-rm -fr cyrus-sasl-*
-tar zxf $DOWNLOADS/cyrus-sasl.tgz
-pushd cyrus-sasl-*
-./configure --prefix=/usr/local
-make
-make install
-popd
-
-# recent krb5
-special_echo "krb5"
-rm -fr krb5-*
-tar zxf $DOWNLOADS/krb5.tgz
-pushd krb5-*/src
-./configure --prefix=/usr/local
-make
-make install
-popd
-
-# recent openldap client
-special_echo "openldap"
-rm -fr openldap-*
-tar zxf $DOWNLOADS/openldap.tgz
-pushd openldap-*
-./configure --prefix=/usr/local --enable-slapd=no --with-cyrus-sasl=yes
-make depend
-make
-make install
-# we need these symlink to build older version of python-ldap
-pushd /usr/local/include
-for f in sasl/*.h ; do ln -s $f ; done
-popd
-popd
-
-# recent libpq for psycopg2
-special_echo "postgres"
-rm -fr postgres-*
-tar zxf $DOWNLOADS/postgresql.tgz
-pushd postgresql-*
-./configure --prefix=/usr/local --without-readline --with-openssl --with-gssapi
-sed -i 's,DEFAULT_PGSOCKET_DIR.*,DEFAULT_PGSOCKET_DIR "/var/run/postgresql",' ./src/include/pg_config_manual.h
-make -C src/bin install
-make -C src/include install || echo "ignoring error"
-make -C src/interfaces install
-popd
+# postgresql for psycopg2
+yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum -y install postgresql13-devel
 
 # recent libxml2 required for lxml
 special_echo "libxml2"
--- download.sh
+++ download.sh
@@ -3,14 +3,6 @@ set -e -x
 rm -fr downloads
 mkdir -p downloads
 pushd downloads
-wget -nv https://github.com/apple/cups/releases/download/v2.3.3/cups-2.3.3-source.tar.gz -O cups.tgz
-#wget -nv ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz -O cyrus-sasl.tgz
-cp ../vendor/cyrus-sasl-2.1.26.tar.gz cyrus-sasl.tgz
-wget -nv https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.4.46.tgz -O openldap.tgz
-wget -nv https://zlib.net/zlib-1.2.11.tar.gz -O zlib.tgz
-wget -nv https://web.mit.edu/kerberos/dist/krb5/1.16/krb5-1.16.1.tar.gz -O krb5.tgz
-wget -nv https://www.openssl.org/source/openssl-1.1.1k.tar.gz -O openssl.tgz
-wget -nv https://ftp.postgresql.org/pub/source/v13.3/postgresql-13.3.tar.gz -O postgresql.tgz
 wget -nv http://xmlsoft.org/sources/libxml2-2.9.12.tar.gz -O libxml2.tgz
 wget -nv http://xmlsoft.org/sources/libxslt-1.1.34.tar.gz -O libxslt.tgz
 popd
