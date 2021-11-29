e8ada4f1efaa640b3617a73bcc13e83cf859d5c5
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-11-29 10:10:41 +0100

    Update requirements fetcher script

--- update-requirements.sh
+++ update-requirements.sh
@@ -9,5 +9,6 @@ wget -O requirements3-odoo11.txt https://raw.githubusercontent.com/odoo/odoo/11.
 wget -O requirements3-odoo12.txt https://raw.githubusercontent.com/odoo/odoo/12.0/requirements.txt
 wget -O requirements3-odoo13.txt https://raw.githubusercontent.com/odoo/odoo/13.0/requirements.txt
 wget -O requirements3-odoo14.txt https://raw.githubusercontent.com/odoo/odoo/14.0/requirements.txt
+wget -O requirements3-odoo15.txt https://raw.githubusercontent.com/odoo/odoo/15.0/requirements.txt
 wget -O requirements3-odoomaster.txt https://raw.githubusercontent.com/odoo/odoo/master/requirements.txt
-cat requirements3-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq > requirements3-latest.txt
+cat requirements3-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq | grep -v suds-jurko > requirements3-latest.txt
