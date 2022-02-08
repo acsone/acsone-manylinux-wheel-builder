69459c02872b227525ed3df733dc64df6e093d3d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-02-08 17:44:41 +0100

    Remove vatnumber from requirements3-latest
    It requires an old setuptools version.

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-wget -O requirements-odoo8.txt https://raw.githubusercontent.com/odoo/odoo/8.0/requirements.txt
-wget -O requirements-odoo9.txt https://raw.githubusercontent.com/odoo/odoo/9.0/requirements.txt
-wget -O requirements-odoo10.txt https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
-cat requirements-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq > requirements-latest.txt
-sed -i "s/pycups/pycups<2/" requirements-latest.txt
-
-wget -O requirements3-odoo11.txt https://raw.githubusercontent.com/odoo/odoo/11.0/requirements.txt
 wget -O requirements3-odoo12.txt https://raw.githubusercontent.com/odoo/odoo/12.0/requirements.txt
 wget -O requirements3-odoo13.txt https://raw.githubusercontent.com/odoo/odoo/13.0/requirements.txt
 wget -O requirements3-odoo14.txt https://raw.githubusercontent.com/odoo/odoo/14.0/requirements.txt
 wget -O requirements3-odoo15.txt https://raw.githubusercontent.com/odoo/odoo/15.0/requirements.txt
 wget -O requirements3-odoomaster.txt https://raw.githubusercontent.com/odoo/odoo/master/requirements.txt
-cat requirements3-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq | grep -v suds-jurko > requirements3-latest.txt
+cat requirements3-*.txt | grep -v "sys_platform == 'win32'" | sed -r -e 's,^([A-Za-z0-9_\.-]+)(.*)$,\1,' | tr A-Z a-z | sort | uniq | grep -v suds-jurko | grep -v vatnumber > requirements3-latest.txt
