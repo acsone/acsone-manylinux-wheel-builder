0745f60f786e62dd0917c2ab37ab600cedddcc8e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-05-12 15:30:06 +0200

    Use altest libxml2 for lxml

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ rm -fr downloads
 mkdir -p downloads
 pushd downloads
 wget -nv https://github.com/apple/cups/releases/download/v2.3.3/cups-2.3.3-source.tar.gz -O cups.tgz
-wget -nv http://xmlsoft.org/sources/libxml2-2.9.10.tar.gz -O libxml2.tgz
+wget -nv http://xmlsoft.org/sources/libxml2-2.9.12.tar.gz -O libxml2.tgz
 wget -nv http://xmlsoft.org/sources/libxslt-1.1.34.tar.gz -O libxslt.tgz
 popd
