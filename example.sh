b8ef25f699c0f844f9c04a310bfe7ec855ef94c3
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-08-01 15:20:15 +0200

    Update example

--- example.sh
+++ example.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e -x
 
-MANYLINUX_VER=manylinux1_x86_64
+MANYLINUX_VER=manylinux_2_24_x86_64
 DOCKER_IMAGE=quay.io/pypa/${MANYLINUX_VER}
-PY_VER=cp27-cp27mu
-REQS=$(ls requirements-*.txt)
+PY_VER=cp310-cp310
+PRE_INSTALL=setuptools
+REQS="requirements3-other.txt requirements3-latest.txt requirements3-oca.txt requirements3-odoo15.txt requirements3-odoomaster.txt"
 
-./download.sh
-docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/build-wheels.sh $PY_VER $REQS
+#./download.sh
+podman run --rm -v `pwd`:/io $DOCKER_IMAGE /io/build-wheels.sh $PY_VER "$PRE_INSTALL" $MANYLINUX_VER $REQS
 ls wheelhouse/
